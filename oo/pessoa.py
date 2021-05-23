c48e85b5ea8632e8d3347f54c01a6fee2d87a3d2
Matheustds0 <tmatheus256@gmail.com>
2021-05-23 14:15:08 -0300

    Finalização do jogo Python Birds do curso Python Pro (obs: a pasta oo foi designada para o aprendizado de orientação a objetos no python, não estando relacionada ao jogo)

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -6,6 +6,9 @@ class Pessoa:
         self.idade = idade
         self.filhos = list(filhos)
 
+    def cumprimentar(self):
+        return f'Olá, meu nome é {self.nome}'
+
     @staticmethod
     def metodo_estatico():
         return 42
@@ -13,9 +16,17 @@ class Pessoa:
     def nome_atributos_classe(cls):
         return f'{cls} - olhos {cls.olhos}'
 
+class Homem(Pessoa):
+    def cumprimentar(self):
+        cumprimentar_classe_pai = super().cumprimentar()
+        return f'{cumprimentar_classe_pai}. Aperto de mão'
+
+class Mutante(Pessoa):
+    olhos = 3
+
 if __name__ == '__main__':
-    renzo = Pessoa(nome = 'Renzo')
-    luciano = Pessoa('Luciano', 49, renzo)
+    renzo = Mutante(nome = 'Renzo')
+    luciano = Homem('Luciano', 49, renzo)
     print(luciano.nome)
     print(luciano.idade)
     for filho in luciano.filhos:
@@ -26,8 +37,15 @@ if __name__ == '__main__':
     del luciano.olhos
     print(luciano.__dict__)
     print(renzo.__dict__)
-    Pessoa.olhos = 3
     print(Pessoa.olhos)
     print(id(Pessoa.olhos), id(luciano.olhos), id(renzo.olhos))
     print(Pessoa.metodo_estatico(), luciano.metodo_estatico())
     print(Pessoa.nome_atributos_classe(), luciano.nome_atributos_classe())
+    pessoa = Pessoa('Anônimo')
+    print(isinstance(pessoa, Pessoa))
+    print(isinstance(pessoa,Homem))
+    print(isinstance(renzo, Pessoa))
+    print(isinstance(renzo, Homem))
+    print(renzo.olhos)
+    print(luciano.cumprimentar())
+    print(renzo.cumprimentar())
